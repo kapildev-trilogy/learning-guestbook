a5a1031ebfd01101a26dd694bb2b14caad783fc0
kapildev-trilogy <kapildev.neupane@trilogy.com>
2022-05-24 09:16:25 +0000

    Expose Hello handler to internet

--- lib/learning-guestbook-stack.ts
+++ lib/learning-guestbook-stack.ts
@@ -1,9 +1,7 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigw from 'aws-cdk-lib/aws-apigateway';
 
 export class LearningGuestbookStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -13,6 +11,10 @@ export class LearningGuestbookStack extends Stack {
       runtime: lambda.Runtime.NODEJS_14_X,
       code: lambda.Code.fromAsset('lambda'),
       handler: 'hello.handler',
+    })
+
+    new apigw.LambdaRestApi(this, 'Endpoint', {
+      handler: hello
     })
   }
 }
